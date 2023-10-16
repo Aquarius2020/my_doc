b7991fbabc5776120b6a07c475cd33c7b3c7e915
Aquarius2020 <2942420554@qq.com>
2023-10-16 19:04:20 +0800

    变量
    存储过程,输入输出
    
    流程控制

--- 学习 mysql/test.sql
+++ 学习 mysql/test.sql
@@ -1,10 +1,18 @@
 -- Active: 1697429850366@@127.0.0.1@3306@vhr
 
+drop Procedure `GET_ALL_TABLES`;
 DELIMITER //
 
 CREATE PROCEDURE GET_ALL_TABLES() 
 BEGIN 
-	show TABLES;
+	-- 声明变量
+	declare val int DEFAULT 0;
+	show tables;
+	-- select 外使用 set
+	set val=1;
+	-- select 内使用 into
+	select count(*) into val from hr;
+	select val;
 end // 
 
 
@@ -12,4 +20,35 @@ DELIMITER ;
 
 call GET_ALL_TABLES()
 
-drop Procedure `GET_ALL_TABLES`;
+
+drop PROCEDURE get_number;
+DELIMITER //
+
+create Procedure get_number(in v1 int, out o int,INOUT tmp int)
+BEGIN
+select "before ",o,tmp;
+IF v1>0 then
+	select count(*) into o from hr where id=v1;
+ELSE
+	set o=-100;
+end IF;
+
+-- 用 if 不如用 case when. 可读性更高
+case 
+	when v1>0 then select "大于 0";
+	when v1<0 then select "小于 0";
+	when v1=0 then select "等于 0";
+end CASE ;
+
+set tmp=12 ;
+select "after ",o,tmp;
+
+END
+DELIMITER ;
+
+
+set @val=1;
+set @val2=2;
+call get_number(3,@val,@va2);
+select @val,@val2;
+
